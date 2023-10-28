87e7c5619cdfa2608a8d216fc1ded8078e3249fe
OwenDing1104 <karsaowen@gmail.com>
2023-10-28 17:35:14 -0400

    Test4

--- src/PolarisChip.js
+++ src/PolarisChip.js
@@ -1,4 +1,5 @@
 import { LitElement, html, css } from 'lit';
+import "./date-chip.js";
 
 export class PolarisChip extends LitElement {
   static get properties() {
@@ -6,9 +7,9 @@ export class PolarisChip extends LitElement {
       name: { type: String },
       link: { type: String },
       active: { type: Boolean, reflect: true },
-      date: { type: String },
-      month: { type: String },
-      day: { type: Number }
+      imageSrc: { type: String },
+      header: { type: String },
+      desc: { type: String },
     };
   }
 
@@ -16,109 +17,131 @@ export class PolarisChip extends LitElement {
     return css`
       :host {
         display: inline-block;
-        margin: 0 16px 16px 0;
-      }
-      .link:focus,
-      .link:hover,
-      :host([active]) .link {
-        background-color: #e4e5e7;
-        color: #005fa9;
-        border: 2px solid #eaefec;
-        border-radius: 2px;
-        cursor: pointer;
-        text-decoration: underline;
+        margin: 0 12px 12px 0;
       }
 
-      .link {
+      .cardcontainer {
+        display: flex;
+        flex-direction: column;
+        float: left;
+        box-sizing: border-box;
+        font-family: 'Roboto', sans-serif;
+        margin-right: 10px;
+        line-height: 24px;
+      }
+
+      a.chip {
+        position: relative;
+        display: block;
+        cursor: pointer;
+      }
+
+      a.chip::before {
+        content: '';
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(30, 64, 124, 0.35); /* Navy blue with 35% opacity */
+        opacity: 0; /* Initially transparent */
+      }
+
+      a.chip:hover::before {
+        opacity: 1; /* Make the overlay fully visible on hover */
+      }
+
+      a.chip img {
+        max-width: 300px;
+        height: auto;
+      }
+
+      a.chip {
         font-weight: bold;
         text-decoration: none;
-        padding: 8px 4px;
-        border: 2px solid #444;
-        color: #444;
+        padding: 0px 0px;
         font-size: 16px;
       }
 
-      .month {
-        background-color: #1E407C;
-        background-clip: border-box;
-        background-origin: padding-box;
-        background-size: auto;
-        border-bottom-left-radius: 2px;
-        border-bottom-right-radius: 2px;
-        border-top-left-radius: 0px;
-        border-top-right-radius: 0px;
-        color: white;
-        font-size: 12.8px;
-        font-weight: 700;
-        line-height: 23.04px;
+      .imagecontainer {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center; /* Center horizontally */
+        align-items: center; /* Center vertically */
+      }
+
+      @media (max-width: 768px) {
+        .cardcontainer {
+          flex-direction: column;
+        }
+      }
+
+      .headercontainer {
+        display: flex;
+        margin-top: 20px;
+        margin-bottom: 20px;
+      }
+
+      h3 {
+        font-size: 20.8px;
+        text-transform: capitalize;
+      }
+
+      .cardheader {
+        color: rgb(0, 95, 169);
+        text-decoration-line: none;
+        text-decoration-style: solid;
+      }
+
+      .descriptioncontainer {
         padding-left: 10px;
         padding-right: 10px;
-        padding-top: 1px;
-        padding-bottom: 1px;
-        text-transform: uppercase;
-      }
-      .day {
-        background-color: white;
-        background-clip: border-box;
-        background-origin: padding-box;
-        background-size: auto;
-        border-bottom-left-radius: 2px;
-        border-bottom-right-radius: 2px;
-        border-top-left-radius: 0px;
-        border-top-right-radius: 0px;
-        color: #444444;
-        font-size: 18px;
-        font-weight: 900;
-        line-height: 27px;
-        padding-left: 20px;
-        padding-right: 20px;
         padding-top: 10px;
         padding-bottom: 10px;
-      }  
+      }
+
+      .description {
+        font-size: 19.2px;
+        color: black;
+      }
     `;
   }
 
   constructor() {
     super();
-    this.name = 'flu shot';
-    this.link = 'https://hr.psu.edu/news/university-reminds-employees-flu-covid-19-vaccines-resources-and-policies';
-    this.date = '01/01/2001'
-    this.month = 'Jan';
-    this.day = 1;
-  }
-
-  updated(changedProperties) {
-    if (changedProperties.has('date')) {
-      this.dateToMonth();
-      this.dateToDay();
-    }
-  }
-
-  dateToMonth() {
-    const inputDate = new Date(this.date);
-    this.month = inputDate.toLocaleDateString('en-us', { month:"short" });
-  }
-
-  dateToDay() {
-    const inputDate = new Date(this.date);
-    this.day = inputDate.toLocaleDateString('en-us', { day:"numeric" });
+    this.name = '';
+    this.link = '';
+    this.imageSrc = '';
+    this.header = '';
+    this.desc = '';
   }
 
   render() {
     return html`
-    <div class="card">
-      <div class="card-header">
-        <div class="date-tag">${this.month} ${this.day}</div>
-        <img src="https://hr.psu.edu/sites/hr/files/styles/homepage_hero/public/2023-10/ExpandedFamilySick_0.jpg?h=5265ff56&itok=vq6ggXyF" alt="flu" class="card-image"/>
+    <div class="cardcontainer">
+    <!-- image container -->
+      <div class="imagecontainer">
+        <a class="chip" href="${this.link}" target="_blank">
+          <img src="${this.imageSrc}" alt="${this.name}">
+          <slot>${this.name}</slot>
+        </a>
       </div>
-      <h1>${this.name}</h1>
-      <div class="content">
-        <a href="${this.link}" class="link">${this.name}</a>
-        <slot></slot>
+      <!-- date chip / heading -->
+      </div class="headercontainer">
+        <date-chip></date-chip>
+        <h3>
+          <a class="cardheader" href="${this.link}" target="_blank">
+            <slot>${this.header}</slot>
+          </a> 
+        </h3>
+      </div>
+      <!-- desc -->
+      <div class="descriptioncontainer">
+        <p class="description">
+          <slot>${this.desc}</slot>
+        </p>
       </div>
     </div>
     `;
   }
 }
-
-customElements.define('polaris-chip', PolarisChip);
